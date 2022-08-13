37ad2913eda4e407f8d3eeb575e15feb1bab3557
naina321 <naina1910078@akgec.ac.in>
2022-08-13 12:05:08 +0530

    setting structure for to-do list

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("css"));
+app.use(express.urlencoded({ extend: true }));
+
+const ex = "working";
+const items = [];
+app.get("/", (req, res) => {
+  res.render("list", { elem: items });
+});
+app.post("/", (req, res) => {
+  const item = req.body.ele1;
+  items.push(item);
+  console.log(req.body.ele1);
+  res.redirect("/");
+});
+app.listen(3000, () => {
+  console.log("serveer started");
+});
